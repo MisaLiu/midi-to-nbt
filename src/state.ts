4ae46df5e305687a78be6c7566b85350500c6bc6
Misa Liu <misaliu@misaliu.top>
2026-02-20 22:28:45 +0800

    refactor: Redo state

--- src/state.ts
+++ src/state.ts
@@ -1,23 +1,50 @@
 import { create } from 'zustand';
+import { combine } from 'zustand/middleware';
 import type { MidiFile } from 'midifile-ts';
 import type { Nullable, Vector3 } from './types';
 
 interface GlobalState {
   midi: Nullable<MidiFile>,
   startPos: Vector3,
-  pianoFacing: Nullable<'east' | 'west' | 'north' | 'south'>,
+  pianoFacing: 'east' | 'west' | 'north' | 'south',
+  fallingHeight: number,
 };
 
-interface GlobalStateWithActions extends GlobalState {
-  setMidi: (midi: MidiFile) => void,
-}
+export const useStore = create(
+  combine(
+    {
+      midi: null,
+      startPos: { x: 0, y: 0, z: 0 },
+      pianoFacing: 'south',
+      fallingHeight: 20,
+    } as GlobalState,
+    (set, get) => ({
+      setMidi: (midi: MidiFile) => {
+        set(() => ({
+          midi: midi,
+        }));
+      },
 
-export const useStore = create<GlobalStateWithActions>((set) => ({
-  midi: null,
-  startPos: { x: 0, y: 0, z: 0 },
-  pianoFacing: null,
+      setStartPos: (axis: 'x' | 'y' | 'z', value: number) => {
+        const pos = get().startPos;
+        pos[axis] = value;
 
-  setMidi: (midi) => set(() => ({
-    midi,
-  })),
-}));
+        set(() => ({
+          startPos: { ...pos },
+        }));
+      },
+
+      setFacing: (facing: 'east' | 'west' | 'north' | 'south') => {
+        set(() => ({
+          pianoFacing: facing,
+        }));
+      },
+
+      setFallingHeight: (height: number) => {
+        set(() => ({
+          fallingHeight: height,
+        }));
+      },
+    })
+  )
+);
